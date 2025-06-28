0b42bb780f813be3cba33dcbbdee96dbeeb8f6a6
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2025-06-29 00:34:30 +0500

    first commit

---
+++ app/main.py
@@ -0,0 +1,38 @@
+"""
+Востроизведение текстовых сообщений
+"""
+
+from gtts import gTTS
+import pygame
+import os
+import time
+
+
+def play_mp3(file_path):
+    pygame.mixer.init()
+    pygame.mixer.music.load(file_path)
+    pygame.mixer.music.play()
+
+    # Ждем окончания воспроизведения
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)
+
+
+SOUND_FILE_NAME = "output"
+IS_DELETE_AFTER_PLAY = False
+# text = "Пожалуйста, идите нахуй!"
+text = b"\xd0\x9f\xd0\xbe\xd0\xb6\xd0\xb0\xd0\xbb\xd1\x83\xd0\xb9\xd1\x81\xd1\x82\xd0\xb0, \xd0\xb8\xd0\xb4\xd0\xb8\xd1\x82\xd0\xb5 \xd0\xbd\xd0\xb0\xd1\x85\xd1\x83\xd0\xb9!"
+language = "ru"
+
+
+try:
+    tts = gTTS(text=text.decode("utf-8"), lang=language, slow=False)
+    tts.save(SOUND_FILE_NAME)
+    # os.system("start output.mp3")
+    play_mp3(SOUND_FILE_NAME)
+    if IS_DELETE_AFTER_PLAY is not False:
+        time.sleep(1)
+        os.remove(SOUND_FILE_NAME)
+
+except Exception as err:
+    raise err
---
+++ voicekidtimer/src/voicekidtimer/app.py
@@ -0,0 +1,26 @@
+"""
+Таймер для детей, который говор каждые 5 минут в обратном порядке, сколько сремени осталось до конца
+"""
+
+import toga
+from toga.style import Pack
+from toga.style.pack import COLUMN, ROW
+
+
+class VoiceKidTimer(toga.App):
+    def startup(self):
+        """Construct and show the Toga application.
+
+        Usually, you would add your application to a main content box.
+        We then create a main window (with a name matching the app), and
+        show the main window.
+        """
+        main_box = toga.Box()
+
+        self.main_window = toga.MainWindow(title=self.formal_name)
+        self.main_window.content = main_box
+        self.main_window.show()
+
+
+def main():
+    return VoiceKidTimer()
