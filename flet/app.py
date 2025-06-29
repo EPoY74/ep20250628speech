f01bac392c3b0344e7d63a8e3f97dc40cd7aefc7
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2025-06-29 10:36:18 +0500

    Зобавил 3 вида вывода, что бы посмотреть как и что есть

--- flet/app.py
+++ flet/app.py
@@ -5,4 +5,11 @@ def main(page: ft.Page):
     page.add(ft.Text("Привет, Flet!"))
 
 
-ft.app(target=main, view=ft.AppView.WEB_BROWSER)  # Запуск в браузере
+# Запуск в браузере
+# ft.app(target=main, view=ft.AppView.WEB_BROWSER)
+
+# Запуск в десктоп окне
+# ft.app(target=main)
+
+# Запуск PWA
+ft.app(target=main, view=ft.AppView.FLET_APP_WEB)
