79ef541d312c9a6210f237abcd6740ac195f393f
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2025-07-02 06:41:00 +0500

    Удалил из проекта непрофильные потуги

--- app/main.py
+++ app/main.py
@@ -18,15 +18,16 @@ def play_mp3(file_path):
         pygame.time.Clock().tick(10)
 
 
-SOUND_FILE_NAME = "output"
+SOUND_FILE_NAME = "output.mp3"
 IS_DELETE_AFTER_PLAY = False
-# text = "Пожалуйста, идите нахуй!"
-text = b"\xd0\x9f\xd0\xbe\xd0\xb6\xd0\xb0\xd0\xbb\xd1\x83\xd0\xb9\xd1\x81\xd1\x82\xd0\xb0, \xd0\xb8\xd0\xb4\xd0\xb8\xd1\x82\xd0\xb5 \xd0\xbd\xd0\xb0\xd1\x85\xd1\x83\xd0\xb9!"
+# text = b"\xd0\x9f\xd0\xbe\xd0\xb6\xd0\xb0\xd0\xbb\xd1\x83\xd0\xb9\xd1\x81\xd1\x82\xd0\xb0, \xd0\xb8\xd0\xb4\xd0\xb8\xd1\x82\xd0\xb5 \xd0\xbd\xd0\xb0\xd1\x85\xd1\x83\xd0\xb9!"
+text = "Миля! Ты хочешь пешком пойти?"
 language = "ru"
 
 
 try:
-    tts = gTTS(text=text.decode("utf-8"), lang=language, slow=False)
+    # tts = gTTS(text=text.decode("utf-8"), lang=language, slow=False)
+    tts = gTTS(text=text, lang=language, slow=False)
     tts.save(SOUND_FILE_NAME)
     # os.system("start output.mp3")
     play_mp3(SOUND_FILE_NAME)
